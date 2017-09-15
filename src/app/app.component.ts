c0883d649550714b7a3c262537a347c192e3cc09
VitalieBraga <vitaliebraga@gmail.com>
2017-09-14 18:11:36 -0700

    finished the random color generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title: string = 'Retro Barcode Generator';
+  many = [1,2,3,4,5,6,7,8,9,10];
+  color = random_color;
+
+   
 }
+
+function random_color(): string {
+  var rgb = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let color = '#'  //this is what we'll return!
+  for (var i = 0; i < 6; i++) {
+    let x = Math.floor((Math.random() * 16))
+    color += rgb[x];
+  }
+  return color;
+};
